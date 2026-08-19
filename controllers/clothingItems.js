8300a985d0afb349717e8caff90ba3dea356129b
Ilias Katsantonis <iliasckatsantonis@gmail.com>
2026-08-19 16:00:52 +0300

    feat: restrict item deletion to owners

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -26,9 +26,14 @@ const createItem = (req, res) => {
 
 const deleteItem = (req, res) => {
   const { itemId } = req.params;
-  ClothingItem.findByIdAndDelete(itemId)
+  ClothingItem.findById(itemId)
     .orFail()
-    .then((item) => res.status(200).send(item))
+    .then((item) => {
+      if (!item.owner.equals(req.user._id)) {
+        return res.status(403).send({ message: "Forbidden" });
+      }
+      return item.deleteOne().then(() => res.status(200).send(item));
+    })
     .catch((err) => {
       console.error(err);
       if (err.name === "CastError") {
