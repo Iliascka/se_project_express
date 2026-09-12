82b39c12fcada6d32e502431165771153b5db3ff
Ilias Katsantonis <iliasckatsantonis@gmail.com>
2026-09-12 13:57:47 +0300

    feat: create Joi validation schemas

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,2 +1,37 @@
 const { Joi, celebrate } = require("celebrate");
 const validator = require("validator");
+
+const validateclothingItemBody = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    imageUrl: Joi.string().required(),
+  }),
+});
+
+const validateUserBody = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    avatar: Joi.string().required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+const validateLogin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+const validateUserId = celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().hex().length(24),
+  }),
+});
+
+const validateItemId = celebrate({
+  params: Joi.object().keys({
+    itemId: Joi.string().hex().length(24),
+  }),
+});
