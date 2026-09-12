509fe70b7a72e0207acfefb590699f1ab629332b
Ilias Katsantonis <iliasckatsantonis@gmail.com>
2026-09-12 15:11:29 +0300

    feat: complete Joi and Celebrate validation middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes");
+const { errors } = require("celebrate");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -16,3 +17,5 @@ mongoose
 app.use(express.json());
 app.use(cors());
 app.use("/", mainRouter);
+app.use(errors());
+// app.use(errorHandler);
--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,17 +1,34 @@
 const { Joi, celebrate } = require("celebrate");
 const validator = require("validator");
 
-const validateclothingItemBody = celebrate({
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.errorr("string.uri");
+};
+
+const validateClothingItemBody = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    imageUrl: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+      "string.empty": 'The "name" field must be filled in',
+    }),
+    imageUrl: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "imageUrl" field must be filled in',
+      "string.uri": 'The "imageUrl" field must be a valid url',
+    }),
   }),
 });
 
 const validateUserBody = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
-    avatar: Joi.string().required(),
+    avatar: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "avatar" field must be filled in',
+      "string.uri": 'The "avatar" field must be a valid url',
+    }),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
@@ -35,3 +52,11 @@ const validateItemId = celebrate({
     itemId: Joi.string().hex().length(24),
   }),
 });
+
+module.exports = {
+  validateClothingItemBody,
+  validateUserBody,
+  validateLogin,
+  validateUserId,
+  validateItemId,
+};
