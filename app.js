e8e597879eb76d400488c5a1bee869bb44efddaa
Ilias Katsantonis <iliasckatsantonis@gmail.com>
2026-09-15 18:54:05 +0300

    chore: configure JWT secret from environment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const mainRouter = require("./routes");
 const { errors } = require("celebrate");
 const errorHandler = require("./middlewares/error-handler");
